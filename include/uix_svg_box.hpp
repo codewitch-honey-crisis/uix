7486562514bff99e1e1c547d721b5af6166a2f75
codewitch-honey-crisis <gaztoast@gmail.com>
2023-03-20 04:40:20 -0700

    added justify to svg_doc

--- include/uix_svg_box.hpp
+++ include/uix_svg_box.hpp
@@ -19,6 +19,7 @@ class svg_box : public uix::control<PixelType, PaletteType> {
     // member data
     gfx::svg_doc* m_svg;
     srect16 m_rect;
+    uix_justify m_justify;
     // no reason for copy semantics
     svg_box(const svg_box& rhs) = delete;
     svg_box& operator=(const svg_box& rhs) = delete;
@@ -27,6 +28,7 @@ class svg_box : public uix::control<PixelType, PaletteType> {
         do_move_control(rhs);
         m_svg = rhs.m_svg;
         m_rect = rhs.m_rect;
+        m_justify = rhs.m_justify;
     }
 
    public:
@@ -48,9 +50,18 @@ class svg_box : public uix::control<PixelType, PaletteType> {
         }
     }
     svg_box(uix::invalidation_tracker& parent, const palette_type* palette = nullptr)
-        : base_type(parent, palette), m_svg(nullptr), m_rect(0,0,0,0) {
+        : base_type(parent, palette), m_svg(nullptr), m_rect(0,0,0,0),m_justify(uix_justify::top_left) {
     }
-
+    uix_justify justify() const {
+        return m_justify;
+    }
+    void justify(uix_justify value) {
+        if(m_justify!=value) {
+            m_justify = value;
+            m_rect = {0,0,0,0};
+            this->invalidate();
+        }
+    }
     virtual void on_paint(control_surface_type& destination, const uix::srect16& clip) override {
         // get the rect for the drawing area
         uix::srect16 b = (uix::srect16)this->dimensions().bounds();
@@ -59,7 +70,41 @@ class svg_box : public uix::control<PixelType, PaletteType> {
         if (m_svg != nullptr) {
             float scale = m_svg->scale(b.dimensions());
             if(m_rect.x1==0&&m_rect.y1==0&&m_rect.x2==0&&m_rect.y2==0) {
-                m_rect = srect16(0,0,m_svg->dimensions().width*scale-1,m_svg->dimensions().height*scale-1);
+                uint16_t w = m_svg->dimensions().width*scale;
+                uint16_t h = m_svg->dimensions().height*scale;
+                m_rect = srect16(0,0,w-1,h-1);
+                switch(m_justify) {
+                    case uix_justify::top_middle:
+                        m_rect.center_horizontal_inplace(((base_type*)this)->bounds());
+                        break;
+                    case uix_justify::top_right:
+                        m_rect.offset_inplace(this->dimensions().width-w,0);
+                        break;
+                    case uix_justify::center_left:
+                        m_rect.center_vertical_inplace(((base_type*)this)->bounds());
+                        break;
+                    case uix_justify::center:
+                        m_rect.center_inplace(((base_type*)this)->bounds());
+                        break;
+                    case uix_justify::center_right:
+                        m_rect.center_vertical_inplace(((base_type*)this)->bounds());
+                        m_rect.offset_inplace(this->dimensions().width-w,0);
+                        break;
+                    case uix_justify::bottom_left:
+                        m_rect.offset_inplace(0,this->dimensions().height-h);
+                        break;
+                    case uix_justify::bottom_middle:
+                        m_rect.center_horizontal_inplace(((base_type*)this)->bounds());
+                        m_rect.offset_inplace(0,this->dimensions().height-h);
+                        break;
+                    case uix_justify::bottom_right:
+                        m_rect.offset_inplace(this->dimensions().width-w,
+                                            this->dimensions().height-h);
+                        break;
+                    default: // top_left
+                    break;
+                }
+                
             }
             if(clip.intersects(m_rect)) {
                 gfx::draw::svg(destination,
@@ -70,6 +115,10 @@ class svg_box : public uix::control<PixelType, PaletteType> {
         }
         // call the base on paint method
         base_type::on_paint(destination, clip);
+    }
+    virtual void bounds(const srect16& value) override {
+        base_type::bounds(value);
+        m_rect = srect16(0,0,0,0);
     }
 };
 }  // namespace uix
